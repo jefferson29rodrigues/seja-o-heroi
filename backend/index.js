5c21f21943b4c4307682eef18dd6126459c0469f
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-01-23 23:51:14 -0300

    app return res.send

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
+
+app.get('/', (request, response) => {
+    return response.send('Hello World!')
+});
 
 app.listen(3333);
